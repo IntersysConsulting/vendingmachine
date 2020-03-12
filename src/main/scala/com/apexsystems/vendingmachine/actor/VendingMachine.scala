e986cc121a46bfac8b579ecc50eb5b1c8799b651
Edgar Parra <edgarjpl@hotmail.com>
2020-03-12 10:11:35 -0600

    Dispense fixed

--- src/main/scala/com/apexsystems/vendingmachine/actor/VendingMachine.scala
+++ src/main/scala/com/apexsystems/vendingmachine/actor/VendingMachine.scala
@@ -8,8 +8,9 @@ case class Selection(input : String)
 case class Money(amount : Double)
 case class Item(name : String, sku : Int, price : Double)
 case class Cancel()
-case class Error(desc : String)
-case class Dispense(position : String, item : Item, sender: ActorRef)
+case class ProcessSelection(input : String, slot : ListBuffer[Item], replyTo: ActorRef)
+case class Dispense(input : String, slot : ListBuffer[Item], replyTo: ActorRef)
+case class Change(replyTo: ActorRef)
 
 class VendingMachine(var currentAmount : Double, slots : Map[String, ListBuffer[Item]]) extends Actor{
   val validAmounts = Array(0.05, 0.10, 0.25, 1, 5)
@@ -17,33 +18,45 @@ class VendingMachine(var currentAmount : Double, slots : Map[String, ListBuffer[
   override def receive: Receive = {
     case Money(amount) =>
       amount match {
-        case amount if validAmounts contains amount => { currentAmount += amount
-          sender() ! s"Current amount $$${currentAmount}" }
+        case amount if validAmounts contains amount =>
+          currentAmount += amount
+          sender() ! s"Current amount $$$currentAmount"
         case _ => sender() ! "Unsupported money"
       }
 
     case Selection(input) =>
       input match {
-        case input if !slots.contains(input) => sender() !  "Invalid selection"
-        case input if slots(input).isEmpty => sender() !  "Item out of stock"
-        case _ => self ! Dispense(input, slots(input)(0), sender())
+        case _ if !slots.contains(input) => sender() ! "Invalid selection"
+        case _ if slots(input).isEmpty => sender() ! "Item out of stock"
+        case _ => self ! ProcessSelection(input, slots(input), sender())
       }
 
-    case Dispense(position, item, sender) =>
+    case ProcessSelection(input, slot, replyTo) =>
+      val item = slot(0)
       item.price match {
-        case price if price > currentAmount =>  sender ! s"Item ${position} costs $$${item.price}"
-        case price if price == currentAmount =>  sender ! s"Dispensed item ${position}"
-        case _ =>  sender ! s"Dispensed change: ${currentAmount}"
+        case _ if currentAmount == 0 => replyTo ! s"Item $input costs $$${item.price}"
+        case price if price > currentAmount =>  replyTo ! "Please insert more money"
+        case price if price <= currentAmount =>
+          currentAmount -= price
+          self ! Dispense(input, slot, replyTo)
       }
 
-    case Cancel =>
-      currentAmount match {
-        case currentAmount if currentAmount > 0 => sender() ! "Dispensed change: ${currentAmount}"
-        case _ => sender() ! "Order canceled"
-      }
+    case Dispense(input, slot, replyTo) =>
+      slot.drop(1)
+      replyTo ! s"Dispensed item $input"
+      self ! Change(replyTo)
+
+    case Change(replyTo)=>
+      if (currentAmount > 0)
+        replyTo ! s"Dispensed change: $$$currentAmount"
+      currentAmount = 0.0
+
+    case Cancel() =>
+      sender() ! "Order canceled"
+      self ! Change (sender)
 
     case _ =>
-      sender() ! "NA"
+      // Ignore
   }
 }
 
--- src/test/scala/com/apexsystems/vendingmachine/actor/VendingMachineTest.scala
+++ src/test/scala/com/apexsystems/vendingmachine/actor/VendingMachineTest.scala
@@ -19,7 +19,7 @@ class VendingMachineTest()
     with BeforeAndAfterAll {
 
   val MAX_SLOT_SIZE = 15
-  //Predefinied list of Items
+  //Predefined list of Items
   val items = List(Item("Snickers", 123123, 2.5), Item("Twizzlers", 459837, 3.0), Item("Twix", 345345, 3.5), Item("Butterfingers", 834578, 4.5), Item("Milky Way", 78656567, 5))
   val itemsSize = items.size
 
@@ -27,32 +27,31 @@ class VendingMachineTest()
   def getItems(): ListBuffer[Item] = ListBuffer[Item]().addAll(LazyList.continually(items(nextInt(itemsSize))).take(nextInt(15)))
 
   //Method to get Slots with Random content
-  def getSlots(): Map[String, ListBuffer[Item]] = Map("A1" -> getItems(),"A2" -> getItems(),"A3" -> getItems(),"A4" -> getItems(),"B1" -> getItems(), "B2" -> ListBuffer(items(1)))
+  def getSlots(): Map[String, ListBuffer[Item]] = Map("A1" -> getItems(),"A2" -> getItems(),"A3" -> getItems(),"A4" -> new ListBuffer[Item],"B1" -> getItems(), "B2" -> ListBuffer(items(1)))
 
   override def afterAll: Unit = {
     TestKit.shutdownActorSystem(system)
   }
 
   "A Vendor Machine actor" must {
-    "send random value" in {
-      val echo = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
-      echo ! "hello world"
-      expectMsg("NA")
+    "Ignore invalid inputs" in {
+      val vendingMachineActor = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
+      vendingMachineActor ! "hello world"
     }
   }
 
   "A Vendor Machine actor" must {
-    "Select empty slot" in {
-      val echo = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
-      echo ! Selection("B3")
+    "Select invalid slot" in {
+      val vendingMachineActor = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
+      vendingMachineActor ! Selection("B3")
       expectMsg("Invalid selection")
     }
   }
 
   "A Vendor Machine actor" must {
     "Select valid item with no money deposited" in {
-      val echo = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
-      echo ! Selection("B2")
+      val vendingMachineActor = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
+      vendingMachineActor ! Selection("B2")
       //receiveWhile(500 millis) {
       //  case msg: String => println(msg)
       //}
@@ -62,16 +61,74 @@ class VendingMachineTest()
 
   "A Vendor Machine actor" must {
     "Select valid item with not enough money deposited" in {
-      val echo = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
-      echo ! Selection("B2")
-      //receiveWhile(500 millis) {
-      //  case msg: String => println(msg)
-      //}
-      expectMsg(s"Item B2 costs $$${items(1).price}")
+      val vendingMachineActor = system.actorOf(Props(new VendingMachine(1.0, getSlots())))
+      vendingMachineActor ! Selection("B2")
+      expectMsg("Please insert more money")
     }
   }
 
+  "A Vendor Machine actor" must {
+    "Select empty slot" in {
+      val vendingMachineActor = system.actorOf(Props(new VendingMachine(1.0, getSlots())))
+      vendingMachineActor ! Selection("A4")
+      expectMsg("Item out of stock")
+    }
+  }
 
+  "A Vendor Machine actor" must {
+    "Deposit valid amount of money " in {
+      val vendingMachineActor = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
+      vendingMachineActor ! Money(1.0)
+      vendingMachineActor ! Money(1.0)
+      expectMsg("Current amount $1.0")
+      expectMsg("Current amount $2.0")
+    }
+  }
+
+  "A Vendor Machine actor" must {
+    "Deposit invalid amount of money " in {
+      val vendingMachineActor = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
+      vendingMachineActor ! Money(10.0)
+      expectMsg("Unsupported money")
+    }
+  }
+
+  "A Vendor Machine actor" must {
+    "Deposit valid amount of money and cancel " in {
+      val vendingMachineActor = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
+      vendingMachineActor ! Money(1.0)
+      vendingMachineActor ! Cancel()
+      expectMsg("Current amount $1.0")
+      expectMsg("Order canceled")
+      expectMsg("Dispensed change: $1.0")
+    }
+  }
+
+  "A Vendor Machine actor" must {
+    "Deposit valid amount of money and select Item with no change" in {
+      val vendingMachineActor = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
+      vendingMachineActor ! Money(1.0)
+      vendingMachineActor ! Money(1.0)
+      vendingMachineActor ! Money(1.0)
+      vendingMachineActor ! Selection("B2")
+      expectMsg("Current amount $1.0")
+      expectMsg("Current amount $2.0")
+      expectMsg("Current amount $3.0")
+      expectMsg("Dispensed item B2")
+      //expectMsg("Dispensed change: $1.0")
+    }
+  }
+
+  "A Vendor Machine actor" must {
+    "Deposit valid amount of money and select Item with change" in {
+      val vendingMachineActor = system.actorOf(Props(new VendingMachine(0.0, getSlots())))
+      vendingMachineActor ! Money(5.0)
+      vendingMachineActor ! Selection("B2")
+      expectMsg("Current amount $5.0")
+      expectMsg("Dispensed item B2")
+      expectMsg("Dispensed change: $2.0")
+    }
+  }
 }
 
 object VendingMachineTest {
